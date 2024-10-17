eb5dd9dd476c28683ac343b6cc01d4927776e94b
hhhscvx <hhhscvx@yandex.ru>
2024-10-17 15:05:55 +0500

    Get tg web data added

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -1,9 +1,80 @@
+import asyncio
+from urllib.parse import unquote
 
-
+from better_proxy import Proxy
 from pyrogram import Client
+from pyrogram.errors import Unauthorized, UserDeactivated, AuthKeyUnregistered
+from pyrogram.raw.functions.messages import RequestWebView
+from pyrogram.errors import FloodWait
+
+from bot.utils import logger
+from bot.config import InvalidSession
 
 
 class Tapper:
     def __init__(self, tg_client: Client) -> None:
         self.session_name = tg_client.name
         self.tg_client = tg_client
+        self.user_id = 0
+
+    async def get_tg_web_data(self, proxy: str | None) -> str:
+        if proxy:
+            proxy: Proxy = Proxy.from_str(proxy)
+            proxy_dict = dict(
+                scheme=proxy.protocol,
+                hostname=proxy.host,
+                port=proxy.port,
+                username=proxy.login,
+                password=proxy.password
+            )
+        else:
+            proxy_dict = None
+
+        self.tg_client.proxy = proxy_dict
+
+        try:
+            with_tg = True
+
+            if not self.tg_client.is_connected:
+                with_tg = False
+                try:
+                    await self.tg_client.connect()
+                except (Unauthorized, UserDeactivated, AuthKeyUnregistered):
+                    raise InvalidSession(self.session_name)
+
+            while True:
+                try:
+                    peer = await self.tg_client.resolve_peer('Yumify_Bot')
+                    break
+                except FloodWait as fl:
+                    fls = fl.value
+
+                    logger.warning(f"{self.session_name} | FloodWait {fl}")
+                    logger.info(f"{self.session_name} | Sleep {fls}s")
+
+                    await asyncio.sleep(fls + 3)
+
+            web_view = await self.tg_client.invoke(RequestWebView(
+                peer=peer,
+                bot=peer,
+                platform='android',
+                from_bot_menu=False,
+                url='https://frontend.yumify.one/'
+            ))
+
+            auth_url = web_view.url
+            query = unquote(string=auth_url.split('tgWebAppData=')[1].split('&tgWebAppVersion')[0])
+
+            self.user_id = (await self.tg_client.get_me()).id
+
+            if with_tg is False:
+                await self.tg_client.disconnect()
+
+            return query
+
+        except InvalidSession as error:
+            raise error
+
+        except Exception as error:
+            logger.error(f"{self.session_name} | Unknown error during Authorization: {error}")
+            await asyncio.sleep(delay=3)
